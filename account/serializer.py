efec50ac468840c84f99e643344b991343a76a34
chi-touch <gloebudav@gmail.com>
2024-06-14 11:21:06 +0100

    account on django

---
+++ account/serializer.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Account
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields =['account_number', 'first_name', 'last_name', 'account_balance', 'account_type']
+
+
+
+    #Note all this commented below can be used in replace of the above Meta class
+    # account_number = serializers.CharField(max_length=10)
+    # first_name = serializers.CharField(max_length=255)
+    # last_name = serializers.CharField(max_length=255)
+    # account_balance = serializers.DecimalField(max_digits=10, decimal_places=2)
+    # account_type = serializers.CharField(max_length=10)

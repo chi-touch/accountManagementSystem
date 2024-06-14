326a0f433b155f492fcf8e41392d362ccefc6d87
chi-touch <gloebudav@gmail.com>
2024-06-14 11:21:27 +0100

    account on django

--- account/models.py
+++ account/models.py
@@ -7,7 +7,53 @@ from .utility import generate_account_number
 
 class Account(models.Model):
     account_number = models.CharField(max_length=10,
-                                      default=generate_account_number, unique=True, primary_key=True)
+                                      default=generate_account_number,
+                                      unique=True,
+                                      primary_key=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     pin = models.CharField(max_length=4)
+    account_balance = models.DecimalField(max_digits=10,
+                                          decimal_places=2,
+                                          default=0.00)
+    ACCOUNT_TYPE = [
+        ('S', 'SAVINGS'),
+        ('C', 'CURRENT'),
+        ('D', 'DOM'),
+
+    ]
+
+    account_type = models.CharField(max_length=1,
+                                    choices=ACCOUNT_TYPE,
+                                    default='S')
+
+
+def __str__(self):
+    return f"{self.first_name} {self.last_name} {self.account_type} {self.account_balance}"
+
+
+class Transaction(models.Model):
+    TRANSACTION_TYPE = [
+        ('DEB', 'DEBIT'),
+        ('CRE', 'CREDIT'),
+        ('TRA', 'TRANSFER')
+    ]
+
+    TRANSACTION_STATUS = [
+        ('S', 'SUCCESSFUL'),
+        ('F', 'FAIL'),
+        ('P', 'PENDING')
+    ]
+
+    account = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    transaction_type = models.CharField(max_length=3,
+                                        choices=TRANSACTION_TYPE,
+                                        default='CRE')
+
+    transaction_time = models.DateTimeField(auto_now=True)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField()
+    transaction_status = models.CharField(max_length=1,
+                                          choices=TRANSACTION_STATUS,
+                                          default='S')
